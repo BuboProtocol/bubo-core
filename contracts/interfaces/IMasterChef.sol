80c4e5ea2a30a0f6872b04b08a7b253cc55f38a0
Bubo Dev <dev@bubo.io>
2021-01-18 14:59:05 -0600

    added masterchef interface

---
+++ contracts/interfaces/IMasterChef.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+interface IMasterChef {
+
+    function airdrop(address _user, uint256 _amount) external;
+
+    function buyInCrowdsale(address _user, uint256 _amount) external;
+
+}
