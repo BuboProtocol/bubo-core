18bb721314090c87734cec9805d6e99aa7047428
Bubo Dev <dev@bubo.io>
2021-01-18 14:59:53 -0600

    added the pre-sale deploy

---
+++ migrations/7_deploy_crowdsale.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require("./BuboToken.sol")
+const Members = artifacts.require('./Members.sol')
+const CrowdSale = artifacts.require('./CrowdSale.sol')
+const MasterChef = artifacts.require('./MasterChef.sol')
+
+module.exports = async function (deployer) {
+    const token = await Token.deployed()
+    const members = await Members.deployed()
+    const masterChef = await MasterChef.deployed()
+    await deployer.deploy(CrowdSale, token.address, members.address, masterChef.address)
+    const crowdSale = await CrowdSale.deployed()
+    await members.addMod(crowdSale.address)
+    await members.addSupport(crowdSale.address)    
+}
